186b19bc30d63ddd3c6607c75164cb36060bbfb9
幻梦晓寒_kaka <97349507+Dreamkaka@users.noreply.github.com>
2023-10-23 23:31:27 +0800

    Update config.php

--- config.php
+++ config.php
@@ -108,10 +108,10 @@ if (CUSTOM_HOST_TW == "CUSTOM_HOST_TW") {
 }
 
 // 指定HTTP或SOCKS5代理
-define('PROXY_ON', 0); // 开启功能。0 否, 1 是
+define('PROXY_ON', 1); // 开启功能。0 否, 1 是
 //define('PROXY_TYPE', "CURLPROXY_HTTP"); // 使用HTTP代理
 define('PROXY_TYPE', "CURLPROXY_SOCKS5"); // 使用SOCKS5代理
-define('PROXY_IP', "127.0.0.1:1234"); // 自行修改地址（web脚本用）
+define('PROXY_IP', "https://bili-api.xiaohan-kaka.gay"); // 自行修改地址（web脚本用）
 define('PROXY_IP_CN', ""); // 自行修改地址（CN）
 define('PROXY_IP_HK', ""); // 自行修改地址（HK）
 define('PROXY_IP_TW', ""); // 自行修改地址（TW）
